b23b0c36854949a8880fdacf9830fdd6a3ba1769
Arnaud Bruckler <a_bruckler@vader.mapado.com>
2015-05-11 15:26:27 +0200

    fix e

--- csv_to_graphml/csv_to_graphml.py
+++ csv_to_graphml/csv_to_graphml.py
@@ -89,7 +89,7 @@ def do_graph_from_file_random(f, sep, outname, maxnbline, pos1, pos2):
         try:
                 G.add_edge(lines[pos1].encode('utf8').strip('\r\n'),
                    lines[pos2].encode('utf8').strip('\r\n'))
-        except Exception:
+        except Exception as e:
                 print 'error e:{0} at line:\n\t{0}'.format(e, line)
         nx.write_graphml(G, outname.split('.')[0] +'.graphml')
 
